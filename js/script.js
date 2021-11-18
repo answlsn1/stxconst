254df2583e68fda2dbf307956b7a9e765644b579
answlsn1 <94333414+answlsn1@users.noreply.github.com>
2021-11-18 15:35:58 +0900

    modal(js파일) 추가
    modal(js파일) 추가

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    // 모달창
+    let modal = $('.modal');
+    let modal_cont = $('.modal-cont');
+    let modal_close = $('.modal-close');
+
+    modal.fadeIn();
+
+    modal_close.click(function () {
+        modal.fadeOut();
+    });
+
+    modal.click(function () {
+        modal.fadeOut();
+    });
+
+    modal_cont.click(function (event) {
+        event.stopPropagation();
+    });
+
+});
